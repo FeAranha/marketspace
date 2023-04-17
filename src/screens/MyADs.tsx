ad9ecc7912eadf1c715d9890b0a26295fad9a5b5
fearanha <felipearanha.c@gmail.com>
2023-04-17 17:51:43 -0300

    MyADs | select

--- src/screens/MyADs.tsx
+++ src/screens/MyADs.tsx
@@ -1,12 +1,77 @@
-import { Center, Icon } from "native-base";
-import { ScreenHeader } from '@components/ScreenHeader';
-
+import {
+  CheckIcon,
+  HStack,
+  Heading,
+  Icon,
+  Select,
+  Stack,
+  Text,
+  VStack,
+} from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import React from "react";
+import { Input } from "@components/Input";
 
 export function MyADs() {
-  return (
-    <Center >
-      <ScreenHeader title="Meus anúncios" variant="plus"/>
+  const [service, setService] = React.useState("");
 
-    </Center>
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const contAD = 9;
+  function goCreateAD() {
+    navigation.navigate("createad");
+  }
+
+  return (
+    <Stack flex={1} bg="gray.7">
+      <HStack
+        w="full"
+        pb={2}
+        pt={12}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Heading ml={115} color="gray.1" fontSize="lg" fontFamily="heading">
+          Meus anúncios
+        </Heading>
+        <Icon
+          as={<AntDesign name="plus" />}
+          size={5}
+          mr="6"
+          color="gray.1"
+          onPress={goCreateAD}
+        />
+      </HStack>
+
+      <VStack p={6}>
+        <HStack justifyContent="space-between">
+          <Text fontSize="sm">{contAD} anúncios</Text>
+
+          <Select
+            borderWidth={1}
+            borderColor='gray.4'
+            height={34}
+            minWidth={112}
+            borderRadius={8}
+            fontSize="sm"
+            selectedValue={service}
+            accessibilityLabel="Filtro"
+            placeholder="Ativos ou Inativos"
+            _selectedItem={{
+              bg: "gray.7",
+              endIcon: <CheckIcon size="5" />,
+            }}
+            onValueChange={(itemValue: React.SetStateAction<string>) =>
+              setService(itemValue)
+            }
+          >
+            <Select.Item label="Todos" value="all" />
+            <Select.Item label="Ativos" value="active" />
+            <Select.Item label="Inative" value="cross" />
+          </Select>
+        </HStack>
+      </VStack>
+    </Stack>
   );
 }
